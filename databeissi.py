63b005c666557cc74a9749c5aa5fa0c0f6a1dbca
Nevapp <pepneval@edu.lapinamk.fi>
2026-03-30 04:18:36 +0300

    Update databeissi.py
    korjaus

--- databeissi.py
+++ databeissi.py
@@ -1,9 +1,8 @@
 import mysql.connector
 def get_connection():
-conn= mysql.connector.connect(
+  conn= mysql.connector.connect(
   host="localhost",
   user="root",
   password="qwerty123",
-  database="ilmanlaatu.mvb"
-)
-return conn
+  database="ilmanlaatu.mvb")
+  return conn
